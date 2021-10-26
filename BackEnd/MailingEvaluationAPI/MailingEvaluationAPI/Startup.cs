356a6c31a7c02da91f3162b7afb1977215516cfd
J. Antonio Cerrillo Rivera <acerrillo@grupogrismar.com>
2021-10-25 20:04:58 -0500

    Added MediatR to services pipeline

--- BackEnd/MailingEvaluationAPI/MailingEvaluationAPI/Startup.cs
+++ BackEnd/MailingEvaluationAPI/MailingEvaluationAPI/Startup.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +35,7 @@ namespace MailingEvaluationAPI
                 });
             });
             services.AddControllers();
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
