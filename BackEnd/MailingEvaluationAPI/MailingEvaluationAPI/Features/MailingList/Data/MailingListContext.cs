62358df483b5935934e7184c3068ac08a9d18aab
J. Antonio Cerrillo Rivera <acerrillo@grupogrismar.com>
2021-10-25 20:09:15 -0500

    Created MailingListContext to use InMemoryDb by default.

---
+++ BackEnd/MailingEvaluationAPI/MailingEvaluationAPI/Features/MailingList/Data/MailingListContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailingEvaluationAPI.Features.MailingList.Data
+{
+    public class MailingListContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseInMemoryDatabase("ml");
+            }
+        }
+    }
+}
