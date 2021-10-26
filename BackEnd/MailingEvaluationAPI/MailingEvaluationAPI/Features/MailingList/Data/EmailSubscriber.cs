f617070e3305666e52d3e1e1a3ab201cd7370dbd
J. Antonio Cerrillo Rivera <acerrillo@grupogrismar.com>
2021-10-25 20:10:44 -0500

    Created basic EmailSubscriber class to store subscriber data.

---
+++ BackEnd/MailingEvaluationAPI/MailingEvaluationAPI/Features/MailingList/Data/EmailSubscriber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailingEvaluationAPI.Features.MailingList.Data
+{
+    public class EmailSubscriber
+    {
+        [Key]
+        public string Email { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
